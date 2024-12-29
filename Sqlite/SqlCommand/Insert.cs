d422c5b5d5508474819dd28aeb19c524a971a486
李静安 <3081309224@qq.com>
2024-12-30 02:11:18 +0800

    添加项目文件。

---
+++ Sqlite/SqlCommand/Insert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sqlite.SqlCommand
+{
+    internal class Insert
+    {
+        public static Insert InsertCommand => new Insert();
+        private string t1 = "INSERT INTO";
+        string _tableName;
+        //
+        private string tableName
+        {
+            get=>_tableName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                _tableName = value;
+            }
+        }
+
+        public string s { get; set; }
+    }
+}
